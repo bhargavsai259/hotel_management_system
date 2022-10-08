745000ecb20484ce24beb31ee3a77a9f1eacddc5
bhargav <102895435+bhargavsai259@users.noreply.github.com>
2022-10-08 23:22:49 +0530

    Add files via upload

---
+++ admin/connect.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "localhost";
+
+$username = "root";
+
+$password = "";
+
+$db = "db_hor";
+
+
+
+// Create connection
+
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+
+
+// Check connection
+
+if (!$conn) {
+
+   die("Connection failed: " . mysqli_connect_error());
+
+}
+
+echo "Connected successfully";
+
+?>
