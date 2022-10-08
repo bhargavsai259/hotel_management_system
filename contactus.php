998608a64e2e6b607ca4cb2cf6ea5fb1572b7b57
bhargav <102895435+bhargavsai259@users.noreply.github.com>
2022-10-08 23:21:28 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Hotel Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Hotel Online Reservation</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">About us</a></li> |
+		<li><a href = "contactus.php">Contact us</a></li> |
+		<li><a href = "gallery.php">Gallery</a></li> |
+		<li><a href = "dineandlounge.php">Dine and Lounge</a></li> |			
+		<li><a href = "reservation.php">Make a reservation</a></li> |
+		<li><a href = "rulesandregulation.php">Rules and Regulation</a></li> |
+		<li><a href ="https://daintiest-tabs.000webhostapp.com/login.php" target="_blank">Chat box</a></li> |
+		<li><a href="https://www.google.com/maps/@17.4292992,78.446592,12z" target="_blank">Maps</a></li>
+	
+		
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>CONTACT US</h3></strong>
+				<br />
+				<br />
+				<center><img src = "images/hotel.jpg" width = "300" height = "300" /></center>
+				<br />
+				<br />
+				<center>
+				<p>Contact No: 9391285045</p>
+				<p>Email: hotelackermans.com</p>
+				</center>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		<label>&copy; Copyright HOR 2022 </label>
+	</div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
